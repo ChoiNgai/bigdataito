b4934a9b81af7bf5b4149b482ae2263413ba909b
CAIWEI <1354280129@qq.com>
2021-06-03 15:25:04 +0800

    项目暂停，调整架构

--- tests/fcn_test.py
+++ tests/fcn_test.py
@@ -1,19 +1,16 @@
 from pyspark.sql import SQLContext, SparkSession
 import read
 
-spark = SparkSession.Builder().appName('rdd').master('local').getOrCreate()
+spark = SparkSession.Builder().appName('bigdataito').master('local').getOrCreate()
 
-# conn = read.mysql(     host='127.0.0.1',
-#                        user='root',             # 数据库用户名
-#                        passwd='yy101597wei',    # 密码
-#                        db='mysql_test',         # 数据库名称
-#                        table = 'student',
-#                        port=3306,
-#                        charset='utf8')
-
-p = ['127.0.0.1','root','yy101597wei', 'mysql_test', 'student',3306,'utf8']
-data = read.mysql(p)
-print(data)
+data = read.mysql(     host='127.0.0.1',        # 主机地址
+                       user='root',             # 数据库用户名
+                       passwd='yy101597wei',    # 密码
+                       db='mysql_test',         # 数据库名称
+                       table = 'student',       # 表名
+                       port=3306,             # 端口号
+                       charset='utf8'          # 编码格式
+                    )
 
 df = spark.createDataFrame(data)    #转换为spakr dataframe
-data = df.rdd                       #转换成rdd
+df.show()
--- tests/read.py
+++ tests/read.py
@@ -1,21 +1,21 @@
 import pymysql
-from pyspark.sql import SparkSession
 
-def mysql(parameter):
+def mysql(host,user,passwd,db,table,port = 3306,charset = 'utf8'):
 
+    parameter = [host ,user ,passwd ,db ,port ,charset] #合并为数组(否则pymysql读取会报错)
     '''连接数据库'''
     conn = pymysql.connect(host = parameter[0],
-                       user = parameter[1],  # 数据库用户名
-                       passwd = parameter[2],  # 密码
-                       db = parameter[3],  # 数据库名称
-                       port = parameter[5],
-                       charset= parameter[6]
-                       )
+                    user = parameter[1],  # 数据库用户名
+                    passwd = parameter[2],  # 密码
+                    db = parameter[3],  # 数据库名称
+                    port = parameter[4],
+                    charset= parameter[5]
+                    )
 
     cursor = conn.cursor()  #创建游标对象
-    table = parameter[4]
-    sql = str( "select * from"+' '+ table  )
+    sql = str( "select * from"+' '+ table  )    # 查询表格所有数据
     cursor.execute(sql)
     result = cursor.fetchall()
 
     return result
+
